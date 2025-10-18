f24cc215edb82a60b53a055ae3406ef25044bee7
Sham Voke <shamvoke@webmeka.com>
2025-10-18 16:24:51 +0300

    🏐 Added navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
+import WebmekaNavbar from "@/components/navbar";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -34,6 +35,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
+            <WebmekaNavbar />
             {children}
           </ThemeProvider>
       </body>
---
+++ src/components/navbar.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import {
+  Navbar,
+  NavBody,
+  NavItems,
+  MobileNav,
+  MobileNavHeader,
+  MobileNavMenu,
+  MobileNavToggle,
+  NavbarLogo,
+  NavbarButton,
+} from "@/components/ui/resizable-navbar";
+import { useState } from "react";
+
+export default function WebmekaNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const navItems = [
+    { name: "Home", link: "/" },
+    { name: "Services", link: "#services" },
+    { name: "Projects", link: "#projects" },
+    { name: "Contact", link: "#contact" },
+  ];
+
+  return (
+    <Navbar className="top-5">
+      {/* Desktop Navbar */}
+      <NavBody className="px-5 py-4">
+        <NavbarLogo />
+        <NavItems items={navItems} />
+        <NavbarButton href="#contact">Contact Us</NavbarButton>
+      </NavBody>
+
+      {/* Mobile Navbar */}
+      <MobileNav className="py-4">
+        <MobileNavHeader>
+          <NavbarLogo />
+          <MobileNavToggle isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
+        </MobileNavHeader>
+        <MobileNavMenu isOpen={isOpen} onClose={() => setIsOpen(false)}>
+          {navItems.map((item, i) => (
+            <a
+              key={i}
+              href={item.link}
+              onClick={() => setIsOpen(false)}
+              className="block text-lg text-neutral-700 dark:text-neutral-200"
+            >
+              {item.name}
+            </a>
+          ))}
+        </MobileNavMenu>
+      </MobileNav>
+    </Navbar>
+  );
+}
