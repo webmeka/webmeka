a98dcf49954d501148acf7f000b611b40e509ffb
Sham Voke <shamvoke@webmeka.com>
2025-10-19 23:12:58 +0300

    🎲 onclick for hero page button

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -65,7 +65,13 @@ export default function HeroSectionOne() {
           }}
           className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
         >
-          <button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+          <button 
+          onClick={() =>
+    document.querySelector("#services")?.scrollIntoView({
+      behavior: "smooth",
+    })
+  }
+          className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
             Explore Now
           </button>
           <button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
