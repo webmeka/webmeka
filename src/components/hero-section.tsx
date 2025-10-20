3af5b205516d05a63820b68dbe8940745481b96f
Sham Voke <shamvoke@webmeka.com>
2025-10-20 18:10:49 +0300

    💬 fixed contact us button

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -74,7 +74,13 @@ export default function HeroSectionOne() {
           className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
             Explore Now
           </button>
-          <button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
+          <button 
+          onClick={() =>
+    document.querySelector("#contact")?.scrollIntoView({
+      behavior: "smooth",
+    })
+  }
+          className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
             Contact Support
           </button>
         </motion.div>
