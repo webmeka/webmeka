147b1b4b00ca5f0535cbd5907e117fe6bd75ecd7
Sham Voke <shamvoke@webmeka.com>
2025-10-21 22:41:08 +0300

    👋 Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,20 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "WEBMEKA",
   description: "A Digital Creative Agency based in Nairobi, Kenya.",
+    icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+    other: [
+      {
+        rel: "manifest",
+        url: "/site.webmanifest",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
