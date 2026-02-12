f6a07834e26a3d2a0223da8bc67c54272e061eba
Sham Voke <shamvoke@webmeka.com>
2026-02-12 23:42:51 +0300

    Add new URLs to sitemap with metadata

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,5 +8,29 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 1,
     },
+    {
+      url: 'https://webmeka.com/services',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://webmeka.com/pricing',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+       {
+      url: 'https://webmeka.com/contact',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://webmeka.com/faqs',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
   ]
 }
