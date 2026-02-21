91f805d23c78df74bb56efc0c5ab54175e3948c9
Sham Voke <shamvoke@webmeka.com>
2026-02-21 23:20:28 +0300

    Update text formatting in features-12 component

--- src/components/features-12.tsx
+++ src/components/features-12.tsx
@@ -35,9 +35,9 @@ export default function Features() {
             <div className="absolute inset-0 -z-10 sm:inset-6 sm:rounded-b-3xl dark:block dark:to-[color-mix(in_oklab,var(--color-zinc-900)_75%,var(--color-background))]"></div>
             <div className="mx-auto max-w-5xl space-y-8 px-6 dark:[--color-border:color-mix(in_oklab,var(--color-white)_10%,transparent)]">
                 <h2 className="text-2xl font-semibold text-neutral-900 dark:text-neutral-100">
-                      <span className="text-webmeka">W</span>hy 
-                      <span className="text-webmeka"> W</span>e 
-                      <span className="text-webmeka"> W</span>eave
+                      <span className="text-webmeka">W</span>HY 
+                      <span className="text-webmeka"> W</span>E 
+                      <span className="text-webmeka"> W</span>EAVE
                 </h2>
 
                 <div className="grid gap-12 sm:px-12 md:grid-cols-2 lg:gap-20 lg:px-0">
