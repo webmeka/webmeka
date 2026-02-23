1e5470b1bb391d515540ebc7db7039cc47464178
Sham Voke <shamvoke@webmeka.com>
2026-02-24 01:42:08 +0300

    Update aria-label and alt attributes in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -31,11 +31,11 @@ export default function FooterSection() {
             <div className="mx-auto max-w-5xl px-6">
                 <Link
                     href="/"
-                    aria-label="go home"
+                    aria-label="go home."
                     className="mx-auto block size-fit">
                     <Image
                     src="/Logo.svg"
-                    alt="Webmeka Logo"
+                    alt="Webmeka Logo."
                     width={80}
                     height={80}
                     priority
